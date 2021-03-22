e21bb14ef73826fff428753b5ab1dc961bbd163e
seunghye0627 <49443130+seunghye0627@users.noreply.github.com>
2021-03-22 20:53:56 +0900

    Seung-hye modified report page and added dbconn.php

---
+++ project/dbconn.php
@@ -0,0 +1,15 @@
+<?php 
+
+$conn = mysqli_connect('localhost', 'mutanxx', '0627','haveit'); 
+// localhost, ID, PW, DB name
+
+
+if($conn) {
+    echo "Success"; //연동 성공
+}else {
+    
+    echo "Error"; //연동 실패
+}
+
+?>
+
