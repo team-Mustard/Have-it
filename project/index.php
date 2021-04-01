97581b9b2d26c865bf4e7e286af5cfccab703072
Song-i <elephant890@naver.com>
2021-04-01 13:21:27 +0900

    song-i linked index with goal.php, weekly.php and monthly.php

--- project/index.php
+++ project/index.php
@@ -1,14 +1,19 @@
 <?php
 $_login = true; // 로그인 안돼있으면 소개 페이지로
-$_page = 0; // 페이지 선택(처음 들어가면 example 페이지가 보이도록)
 if($_login == false){
     include_once "intro.php";
 }
 else {
     include_once "head.php";
     include_once "leftside.php";
-    if($_page == 0){
+    if(empty($_GET['page'])){
         include_once "example.php";
+    } else if($_GET['page'] == 'goal') {
+        include_once "goal.php";
+    } else if($_GET['page'] == 'monthly') {
+        include_once "monthly.php";
+    } else if($_GET['page'] == 'weekly') {
+        include_once "weekly.php";
     }
     include_once "rightside.php";
     include_once "bottom.php";
