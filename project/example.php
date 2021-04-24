b48eb498c1c571bdf4ced9253aaee45256989db0
Song-i <elephant890@naver.com>
2021-04-24 11:18:57 +0900

    song-i adjust example image rate.

--- project/example.php
+++ project/example.php
@@ -2,7 +2,7 @@
 </div>
 <div class="main col-md-8 col-md-offset-2">
     <div class="page-header no-drag text-center">
-        <img width="850px" src="img/example-leftside.png">
+        <img width="790px" src="img/example-leftside.png">
     </div>
 </div>
 <div class="col-md-1">
