5244d2305c45d05855a4d51f7fba490f2702b60b
elephantLikesApple <47274472+elephantLikesApple@users.noreply.github.com>
2021-05-28 21:37:27 +0900

    song-i modified goal_set UI.

--- project/goal_set.php
+++ project/goal_set.php
@@ -4,9 +4,12 @@
 <div class="col-md-1">
     <!-- 빈 공간 -->
 </div>
-<div class="main col-md-7 col-md-offset-2">
-    <div style="margin:25px 10%;">
-        <h2 style="margin-bottom:25px;">목표 추가하기</h2>
+<div class="main col-md-8 col-md-offset-2">
+    
+    <h3><b>목표 추가하기</b></h3>
+    <hr style="border:0; margin-bottom:25px; height:3px; background: #04005E;">
+    
+    <div style="margin:20px 20%;" class="text-center">
         <form class="goal_set_form" name="goal_set_form" method="post" action="db/goal_save.php">
             <input id="goal_name" name="goal_name" type="text" placeholder="새로운 목표 이름" required/><br/>
             
@@ -27,6 +30,8 @@
             </span>
             </p>            
             
+            <hr id="between"> 
+            
             <div id="div_routine0">
                 <!-- <input name="routine_name<?=$routine_num?>" class="routine_name" type="text" placeholder="루틴 이름" required/> <input type="color"><br>
 
@@ -42,7 +47,7 @@
             </div>
             
             <input id="plus_btn" type="button" value="+ 루틴 추가하기" onclick="plus_routine()"><br/><br/>
-            <p style="float:right; margin-right: 120px;">
+            <p style="float:right; margin-right: 80px;">
                 <input type="reset" value="삭제">
                 <input type="button" onclick="goal_set_submit()" value="저장">
             </p>
@@ -71,7 +76,7 @@
     
     function plus_routine() {
       var content = document.getElementById("div_routine"+routine_num);
-      var new_routine = '<input name="routine_name'+routine_num+'" class="routine_name" type="text" placeholder="루틴 이름" required/> <input type="color" name="colors'+routine_num+'"><br><p style="margin-bottom:10px;" required>주기　:<input id="mon'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="mon"><label for="mon'+routine_num+'">월</label><input id="tue'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="tue"><label for="tue'+routine_num+'">화</label><input id="wed'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="wed"><label for="wed'+routine_num+'">수</label><input id="thu'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="thu"><label for="thu'+routine_num+'">목</label><input id="fri'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="fri"><label for="fri'+routine_num+'">금</label><input id="sat'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="sat"><label for="sat'+routine_num+'">토</label><input id="sun'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="sun"><label for="sun'+routine_num+'">일</label><br/></p>';     
+      var new_routine = '<input name="routine_name'+routine_num+'" class="routine_name" type="text" placeholder="루틴 이름" required/> <input type="color" name="colors'+routine_num+'"><br><p style="margin-bottom:10px;">주기　:<input id="sun'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="sun"><label for="sun'+routine_num+'">일</label><input id="mon'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="mon"><label for="mon'+routine_num+'">월</label><input id="tue'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="tue"><label for="tue'+routine_num+'">화</label><input id="wed'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="wed"><label for="wed'+routine_num+'">수</label><input id="thu'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="thu"><label for="thu'+routine_num+'">목</label><input id="fri'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="fri"><label for="fri'+routine_num+'">금</label><input id="sat'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="sat"><label for="sat'+routine_num+'">토</label><br/></p>';     
       routine_num++;
       new_routine += '<div id="div_routine'+routine_num+'"></div>';
       content.innerHTML += new_routine;
