ef91654a7ceb69d575313cdb8b2da44ae7b74c79
elephantLikesApple <47274472+elephantLikesApple@users.noreply.github.com>
2021-05-28 21:04:51 +0900

    song-i repaired container error of footer

--- project/bottom.php
+++ project/bottom.php
@@ -8,12 +8,12 @@
             　|　
             <a id="bg_mode" href="javascript:void(0);" onclick="bg_mode_change(this)">[다크모드]</a>
         </div>
-        <div class="container text-left">
+        <div class="text-left">
             <p>Front-end Developer : 이송이 <i class="fab fa-instagram"> lxx_s2</i> <i class="fab fa-github"> elephantLikesApple</i>
             <br>Back-end Developer : 이주영 <i class="fab fa-instagram"> zerozzu.0_0</i> <i class="fab fa-github"> jyoungl</i>
             　최승혜 <i class="fab fa-instagram"> iamwinxye</i> <i class="fab fa-github"> seunghye0627</i></p>
         </div>
-        <div class="container text-left">
+        <div class="text-left">
         ver. : 1.1.0 <a class="github" href="https://github.com/team-Mustard/Have-it" target="_blank"><i class="fab fa-github"></i></a>
         </div>
     </div>
