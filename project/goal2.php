d56c52bae3c5683be613ffbef1d6aa4b008d3301
jyoungl <pmis118@naver.com>
2021-09-26 22:55:26 +0900

    juyoung goalDelete/code revise

--- project/goal2.php
+++ project/goal2.php
@@ -3,27 +3,28 @@
 
 <?php
 
-// 여기서 DB 처리해주세용
+include "./db/dbconn.php";
+if(isset($_SESSION['userid'])) $userid = $_SESSION['userid'];
 
-$goalID = "1";
-$routineID = ['a', 'b', 'c'];
+if(isset($_GET['goalID'])) $goalID = $_GET['goalID'];
+$goal = "SELECT * FROM goal WHERE goalID = '$goalID'";              
+$result = mysqli_query($conn, $goal);
+$row = mysqli_fetch_array($result);
 
-$goalName = "목표 이름입니다.";
-$term_s = "2020-05-05";
-$term_e = "2020-05-05";
+$goalName = $row['goalName'];
+$term_s = $row['startTerm'];
+$term_e = $row['endTerm'];
 $term_s_dot = str_replace("-", ".", $term_s);
 $term_e_dot = str_replace("-", ".", $term_e);
 
-$color = "#ff00ff";
-$routineName = "루틴 이름입니다.";
 $lastID = 1;
 
 ?>
 
 <div class="main col-md-8 col-md-offset-2">
-    <form method="post" action="./db/goalmodify.php">
+    <form name="goalForm" method="post" action="./db/goalmodify.php">
         <!-- goal id -->
-        <input type="text" value="<?=$goalID?>" style="display:none;">
+        <input type="text" name="goalID" value="<?=$goalID?>" style="display:none;">
         
         <div class="goalNameZone">
             <!-- goal edit -->
@@ -47,88 +48,111 @@ $lastID = 1;
 
         <!-- buttons --> 
         <div class="right">
-            <input value="목표 삭제" id="goalRemove" class="word_4_btn bg_gray_btn round_btn" type="submit" />
+            <input value="목표 삭제" id="goalRemove" onclick="goalDelete(1);" class="word_4_btn bg_gray_btn round_btn" type="button" />
             <input value="목표 수정" id="goalModyfy" onclick="modify();" class="word_4_btn bg_purple_btn round_btn" type="button"/>
-            <input value="수정 완료" id="goalSubmit" class="word_4_btn bg_purple_btn round_btn modi_form" type="submit"> 
+            <input value="수정 완료" id="goalSubmit" onclick="goalDelete(2);" class="word_4_btn bg_purple_btn round_btn modi_form" type="button"> 
         </div>
 
         <div class="clear"></div>
 
         <!-- routine (for) -->
-
-        <div class="routine additional_space">
-            <!-- routine id -->
-            <input type="text" value="<?=$routineID?>" style="display:none;">
-            
-            <!-- print -->
-            <div class="routinePrint">
-                <div class="routineIcon text-center left">
-                    <i class="fas fa-tools" style="font-size:30px; color: <?=$color?>;"></i>
-                    <p style="color: <?=$color?>;"><?=$color?></p>
-                </div>
-                <span class="routineName left">
-                    <p class="fa-2" style="color:<?=$color?>;"><?=$routineName?></p>
-                </span>
-                <div id="basket<?=$routineID?>" class="routineBasket clear" style="background-color: <?=$color?>; height: 50px;">
-                </div>
-            </div>
-
-            <!-- edit -->
-            <div class="routineEdit modi_form goal_set_form">
-                <input value="<?=$routineName?>" type="text" class="routine_name"/>
-                <input type="color" name="colors<?=$routineID?>" value="<?=$color?>">
-                <input id="delete" class="rou" type="submit" name="mode" value="x"/>
-                <p style="margin-bottom:10px;">주기　:
-                    <input id="sun<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="sun"><label for="sun<?=$routineID?>">일</label>
-                    <input id="mon<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="mon"><label for="mon<?=$routineID?>">월</label>
-                    <input id="tue<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="tue"><label for="tue<?=$routineID?>">화</label>
-                    <input id="wed<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="wed"><label for="wed<?=$routineID?>">수</label>
-                    <input id="thu<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="thu"><label for="thu<?=$routineID?>">목</label>
-                    <input id="fri<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="fri"><label for="fri<?=$routineID?>">금</label>
-                    <input id="sat<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="sat"><label for="sat<?=$routineID?>">토</label>
-                </p>
-            </div>
-        </div>
-
-        <div class="routine additional_space">
-            <!-- routine id -->
-            <input type="text" value="<?=$routineID?>" style="display:none;">
-            
-            <!-- print -->
-            <div class="routinePrint">
-                <div class="routineIcon text-center left">
-                    <i class="fas fa-tools" style="font-size:30px; color: <?=$color?>;"></i>
-                    <p style="color: <?=$color?>;"><?=$color?></p>
-                </div>
-                <span class="routineName left">
-                    <p class="fa-2" style="color:<?=$color?>;"><?=$routineName?></p>
-                </span>
-                <div id="basket<?=$routineID?>" class="routineBasket clear" style="background-color: <?=$color?>; height: 50px;">
-                </div>
-            </div>
-
-            <!-- edit -->
-            <div class="routineEdit modi_form goal_set_form">
-                <input value="<?=$routineName?>" type="text" class="routine_name"/>
-                <input type="color" name="colors<?=$routineID?>" value="<?=$color?>">
-                <input id="delete" class="rou" type="submit" name="mode" value="x"/>
-                <p style="margin-bottom:10px;">주기　:
-                    <input id="sun<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="sun"><label for="sun<?=$routineID?>">일</label>
-                    <input id="mon<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="mon"><label for="mon<?=$routineID?>">월</label>
-                    <input id="tue<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="tue"><label for="tue<?=$routineID?>">화</label>
-                    <input id="wed<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="wed"><label for="wed<?=$routineID?>">수</label>
-                    <input id="thu<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="thu"><label for="thu<?=$routineID?>">목</label>
-                    <input id="fri<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="fri"><label for="fri<?=$routineID?>">금</label>
-                    <input id="sat<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="sat"><label for="sat<?=$routineID?>">토</label>
-                </p>
-            </div>
-        </div>
+        <?php
+        $routine = "SELECT * FROM Routine WHERE goalID ='$goalID'";
+        $result2 = mysqli_query($conn, $routine);
         
+        while($row2 = mysqli_fetch_array($result2)){
+            $routineID = $row2['routineID'];
+            $color = $row2['color'];
+            $routineName = $row2['routineName'];
+            $Inter = $row2['rInterval'];
+            $rInter = explode(';', $Inter);
+            
+        echo '    
+        <div class="routine">
+            <!-- routine id -->
+            <input type="text" value="'.$routineID.'" style="display:none;">
+            
+            <!-- print -->
+            <div class="routinePrint">
+                <div class="routineIcon text-center left">
+                    <i class="fas fa-tools" style="font-size:30px; color: '.$color.';"></i>
+                    <p style="color: '.$color.';">'.$color.'</p>
+                </div>
+                <span class="routineName left">
+                    <p class="fa-2" style="color:'.$color.';">'.$routineName.'</p>
+                </span>
+                <div id="basket'.$routineID.'" class="routineBasket clear" style="background-color: '.$color.'; height: 50px;">';
+                
+                /* 루틴 시작후 일주일 지났을 때 마다 check 초기화 */
+                $today = date("Y-m-d");
+                $start_yoli = date('w', strtotime($term_s)); //시작한 날의 요일
+                $today_yoli = date('w', strtotime($today)); //현재 요일
+                
+                if($start_yoli == $today_yoli && $term_s != $today){
+                    //시작한 요일과 현재 요일이 같고 && 지금이 시작일이 아니라면
+                    $up = "update routine set habbitTracker='0' where routineID = '$routineID'";
+                    $upresult = $conn->query($up); }
+            
+                $check = $row2['habbitTracker'];
+            
+                $dayNum = 0;
+                $bcount = 0;
+            
+                while($dayNum<7){
+                    $IntervalNum = $row2['rInterval'];
+                    $interval = explode(';', $IntervalNum);
+                    
+                    if($interval[$dayNum] == 1){
+                        $bcount += 1;
+                       echo '<div class="col-xs-1 col-md-1 con">';
+                       if($check>0){
+                           echo '<i id="jew" class="fa fa-trophy fa-2x" style="margin-top:3px; margin-left:3px; color:'.$color.'; aria-hidden="true"></i>';
+                            $check--;
+                            }                       
+                       echo '</div>'; 
+                    }
+                $dayNum += 1;    
+                }
+            
+            echo '
+                
+                </div>
+            </div>
+            
+            <!-- edit -->
+            <div class="routineEdit modi_form goal_set_form">
+                <input value="'.$routineName.'" type="text" class="routine_name"/>
+                <input type="color" name="colors'.$routineID.'" value="'.$color.'">
+                <input id="delete" class="rou" type="submit" name="mode" value="x"/>
+                <p style="margin-bottom:10px;">주기　:
+                    
+                
+                    <input id="sun'.$routineID.'" type="checkbox"';
+                    echo ($rInter[0]==1 ? 'checked' : ''); echo ' name="routine'.$routineID.'[]" value="sun"><label for="sun'.$routineID.'">일</label>
+                    <input id="mon'.$routineID.'" type="checkbox"';
+                    echo ($rInter[1]==1 ? 'checked' : ''); echo '  name="routine'.$routineID.'[]" value="mon"><label for="mon'.$routineID.'">월</label>
+                    <input id="tue'.$routineID.'" type="checkbox"';
+                    echo ($rInter[2]==1 ? 'checked' : ''); echo '
+                    name="routine'.$routineID.'[]" value="tue"><label for="tue'.$routineID.'">화</label>
+                    <input id="wed'.$routineID.'" type="checkbox"';
+                    echo ($rInter[3]==1 ? 'checked' : ''); echo ' name="routine'.$routineID.'[]" value="wed"><label for="wed'.$routineID.'">수</label>
+                    <input id="thu'.$routineID.'" type="checkbox"';
+                    echo ($rInter[4]==1 ? 'checked' : ''); echo ' name="routine'.$routineID.'[]" value="thu"><label for="thu'.$routineID.'">목</label>
+                    <input id="fri'.$routineID.'" type="checkbox"';
+                    echo ($rInter[5]==1 ? 'checked' : ''); echo ' name="routine'.$routineID.'[]" value="fri"><label for="fri'.$routineID.'">금</label>
+                    <input id="sat'.$routineID.'" type="checkbox"';
+                    echo ($rInter[6]==1 ? 'checked' : ''); echo ' name="routine'.$routineID.'[]" value="sat"><label for="sat'.$routineID.'">토</label>
+                </p>
+            </div>
+        </div>';
+        }
+    ?>
         <!-- routine add button and logic -->
         <div class="routinePlus modi_form goal_set_form">
             <input id="plus_btn" value="+ 루틴 추가하기" onclick="addRoutine(<?=$lastID?>);" type="button" />
             <div id="bas<?=$lastID?>"></div>
         </div>
+    
     </form>
 </div>
 </div>
@@ -152,7 +176,7 @@ $lastID = 1;
         document.getElementById("goalModyfy").classList += ' modi_form';
 
     }
-
+    
     function addRoutine(routine_num) {
         var newRoutine = document.getElementById("bas"+routine_num);
 
@@ -179,4 +203,15 @@ $lastID = 1;
         form += '<div id="bas'+routineSpace+'"></div>';
         newRoutine.innerHTML += form;
     }
+    
+    function goalDelete(index){
+        if (index == 1){
+            document.goalForm.action='./db/goaldelete.php';
+        }
+        else if(index == 2){
+            document.goalForm.action='./db/goalmodify.php';
+        }
+        document.goalForm.submit();
+    }
+    
 </script>
---
+++ project/옛날꺼goal.php
@@ -0,0 +1,242 @@
+<div class="col-md-1">
+</div>
+<div class="main col-md-8 col-md-offset-2">    
+              
+    <?php
+        include "./db/dbconn.php";
+        if(isset($_GET['goalID'])) $goalID = $_GET['goalID'];
+        $goal = "SELECT * FROM goal WHERE goalID = '$goalID'";              
+        
+        $result = mysqli_query($conn, $goal);
+        
+        $row = mysqli_fetch_array($result);
+        $goalName = $row['goalName'];
+        $term_s = $row['startTerm'];
+        $term_e = $row['endTerm'];
+    
+            //목표 수정 폼
+            echo '<form name="goal_modify_form" method="post" action="db/goalmodify.php">';
+            echo '<input style="display:none" id="goalID" type="text" name="goalID" value="'.$goalID.'">';        
+    
+            echo '<div id="goalName" style="margin-top:20px;"><h3 style="display:inline;"><b>'.$goalName.'</b></h3>
+            <div id="goal_term" style="float:right;">'.$term_s.' ~ '.$term_e.'</div>
+            </div><hr style="border:0; height:3px;background: #04005E;">';
+    
+            echo '</form>';
+        ?>
+
+            <form method="post" action="db/goalmodify.php">
+                <input type="button" class="word_4_btn bg_purple_btn round_btn" id="gmodify" style="float:right;" name="gmodify" onclick="goal_modify()" value="목표 수정"/>
+                <input style="float:right;" class="word_4_btn bg_gray_btn round_btn" type="submit" name="test" id="test" value="목표 삭제"/>
+            </form>
+    
+        <?php
+            function delete(){
+                include "./db/dbconn.php";
+                if(isset($_GET['goalID'])) $goalID = $_GET['goalID'];
+                $goalDelete = "DELETE FROM goal WHERE goalID = '$goalID'";
+                    
+                if($conn->query($goalDelete) == TRUE){
+                    echo("<script> alert('목표가 삭제되었습니다.');</script>");
+                    /*$before = $goalID - 1;
+                    echo "<script>location.href='goal.php?goalID=<?=$before?>'</script>";*/
+                }
+            }
+    
+            if(array_key_exists('test', $_POST)){
+                delete();
+            }
+
+            $routine = "SELECT * FROM Routine WHERE goalID ='$goalID'";
+            $result2 = mysqli_query($conn, $routine);
+            $bid = 0;    
+            $routineCount = 0;
+            while($row2 = mysqli_fetch_array($result2)){
+                $routineCount++;
+                $routineID = $row2['routineID'];
+                $routineName = $row2['routineName'];
+                $color = $row2['color'];
+                $tRoutine = "SELECT * FROM t_routine WHERE routineID = '$routineID'";
+                $result3 = mysqli_query($conn, $tRoutine);
+                
+            //루틴 제목 출력
+            echo '<div class="container cart no-modify" style="margin-top: 20px;">
+          <div class="con1 text-center" style="float: left; margin-top: 12px; margin-right: 15px;">
+              <i class="fas fa-tools" style="font-size:30px; color:'.$color.';"></i>
+              <p style="color: '.$color.';">'.$color.'</p>
+          </div>
+          <div class="con1" style="margin-top: 8px; float: left; line-height: 45px;">
+              <p class="fa-2" style="border:none; color:'.$color.'">'.$row2['routineName'].'</p>
+          </div>
+            
+        </div>
+        
+
+        
+        <form class="goal_set_form" name="goal_set_form" method="post" action="db/goalmodify.php">
+            <div class="container cart modi_form">
+            <input name="routine_name'.$routineID.'" class="routine_name" type="text" value="'.$routineName.'" required/>   
+            <input type="color" name="colors'.$routineID.'" value="'.$color.'">
+            <input style="display:none;" type="text" name="Bcount" value="'.$bid.'">
+            <input style="display:none;" type="text" name="rouID'.$bid.'" value="'.$routineID.'">
+            <input type="submit" style="display:none;" name="mode" class="g_btn rou" id="delete" value="x"/>';
+              
+?> 
+        
+             <p style="margin-bottom:10px;">주기　:
+             <input id="sun<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="sun"><label for="sun<?=$routineID?>">일</label>
+             <input id="mon<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="mon"><label for="mon<?=$routineID?>">월</label>
+             <input id="tue<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="tue"><label for="tue<?=$routineID?>">화</label>
+             <input id="wed<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="wed"><label for="wed<?=$routineID?>">수</label>
+             <input id="thu<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="thu"><label for="thu<?=$routineID?>">목</label>
+             <input id="fri<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="fri"><label for="fri<?=$routineID?>">금</label>
+             <input id="sat<?=$routineID?>" type="checkbox" name="routine<?=$routineID?>[]" value="sat"><label for="sat<?=$routineID?>">토</label>
+             </p>
+            </div>
+
+        <?php
+            $r_last = "SELECT * FROM routine ORDER BY routineID DESC LIMIT 1";
+            $r_result = mysqli_query($conn, $r_last);
+            $r_row = mysqli_fetch_array($r_result);
+            $last_ID = $r_row['routineID'];
+            $last_ID += 1;
+        ?>
+
+        <!--해빗 트래커 칸 생성-->
+        <div id="basket<?=$routineID?>" class="container cart no-modify" style="background-color: <?=$color?>;">
+                        
+            <!--루틴 수정 버튼 누를 시 데이터 넘겨줌-->
+            <input style="display:none" id="goalName" type="text" name="goalName" value="<?=$goalName?>">
+            <input style="display:none" id="routineNum" type="text" name="routineNum" value="<?=$routineID?>">
+            <div class="row incart no-gutters">
+            
+                
+               <?php
+                /* 루틴 시작후 일주일 지났을 때 마다 check 초기화 */
+                $today = date("Y-m-d");
+                $start_yoli = date('w', strtotime($term_s)); //시작한 날의 요일
+                $today_yoli = date('w', strtotime($today)); //현재 요일
+                
+                if($start_yoli == $today_yoli && $term_s != $today){
+                    //시작한 요일과 현재 요일이 같고 && 지금이 시작일이 아니라면
+                    $up = "update routine set habbitTracker='0' where routineID = '$routineID'";
+                    $upresult = $conn->query($up); }
+            
+                $check = $row2['habbitTracker'];
+            
+                $dayNum = 0;
+                $bcount = 0;
+            
+                while($dayNum<7){
+                    $IntervalNum = $row2['rInterval'];
+                    $interval = explode(';', $IntervalNum);
+                    
+                    if($interval[$dayNum] == 1){
+                        $bcount += 1;
+                       echo '<div class="col-xs-1 col-md-1 con">';
+                       if($check>0){
+                           echo '<i id="jew" class="fa fa-trophy fa-2x" style="margin-top:3px; margin-left:3px; color:'.$color.'; aria-hidden="true"></i>';
+                            $check--;
+                            }                       
+                       echo '</div>'; 
+                    }
+                $dayNum += 1;    
+                }
+            
+                ?>
+                
+            </div>
+        </div>
+          
+
+<!-- 루틴 삭제 -->
+  <?php 
+        $bid++;
+        }
+    
+    ?>
+    
+    <div class="container cart additional_space">
+    <input id="plus_btn" style="display:none;" type="button" value="+ 루틴 추가하기" onclick="dis(<?=$last_ID?>)">
+
+    <div id="bas<?=$last_ID?>">
+    </div>
+    </div>
+    <div id="nulldiv"></div>
+
+    <input style="display:none;" type="text" name="count" value="<?=$bid?>">
+    <input style="display:none;" type="text" name="GOALID" value="<?=$goalID?>">
+    <input type="submit" id="rousave" class="word_2_btn .bg_indigo_btn .round btn" style="display:none;" onclick="count_return()" name="mode" value="루틴 저장">
+    </form>
+
+    </div>
+    
+    <script>
+        var btn_count = 0; //추가된 루틴 수
+        
+        function dis(routine_num) {
+            routine_num += btn_count;
+            var content = document.getElementById("bas"+routine_num);
+            
+            var new_routine = '<input name="routine_name'+routine_num+'" class="routine_name" type="text" placeholder="루틴 이름" required/> <input type="color" name="colors'+routine_num+'"><br><p style="margin-bottom:10px;" required>주기　:<input id="mon'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="mon"><label for="mon'+routine_num+'">월</label><input id="tue'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="tue"><label for="tue'+routine_num+'">화</label><input id="wed'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="wed"><label for="wed'+routine_num+'">수</label><input id="thu'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="thu"><label for="thu'+routine_num+'">목</label><input id="fri'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="fri"><label for="fri'+routine_num+'">금</label><input id="sat'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="sat"><label for="sat'+routine_num+'">토</label><input id="sun'+routine_num+'" type="checkbox" name="routine'+routine_num+'[]" value="sun"><label for="sun'+routine_num+'">일</label>';     
+            
+            new_routine += '<input name="plusID'+btn_count+'" type="text" style="display:none;" value="'+routine_num+'">';
+            
+            btn_count++;
+            var ssibal = routine_num + btn_count;
+            new_routine += '<div id="bas'+ssibal+'"></div>';
+            content.innerHTML += new_routine;
+    }
+        
+        function count_return(){
+            var con = document.getElementById("nulldiv");
+            
+            var plus_con = '<input name="btn_count" type="text" value="'+btn_count+'" style="display:none;">';
+            
+            con.innerHTML += plus_con;
+    
+        }
+        
+        function goal_modify(){
+            var modify_name = document.getElementById("goalName");
+            var goal = '<input id="goal_name" name="goal_name" type="text" value="<?=$goalName?>" />';
+            modify_name.innerHTML ='';
+            modify_name.innerHTML += goal;
+
+            var gmodify_btn = document.getElementById("gmodify");
+            gmodify_btn.style = "display:none;";
+            
+            var term = document.getElementById("goal_term");
+            var goalTerm = '<label style="font-weight:normal;"><input type="date" value="<?=$term_s?>" name="term_s_date" style="margin-left: 60px; margin-top:10px;"> 부터 </label><label style="margin-left: 20px; font-weight:normal;"><input type="date" value="<?=$term_e?>" name="term_e_date" style="margin-top:10px;"> 까지</label>';
+            
+            goalTerm += '<input type="submit" class="word_2_btn .bg_indigo_btn .round btn" style="margin-top: 20px; float:right;" name="mode" value="저장">';
+            goalTerm += '<input type="button" class="word_2_btn .bg_gray_btn .round btn" style="float:right; margin-top: 20px;" onclick="javascript:location.reload(true)" value="취소">';
+            modify_name.innerHTML += goalTerm;
+            
+            var rou_mod_btn = document.getElementsByClassName("modi_form");
+            var rou_del_btn = document.getElementsByClassName("rou");
+            
+            for(var i = rou_mod_btn.length-1 ; i >= 0; i--) {
+                rou_mod_btn[i].classList.add("additional_space");
+                rou_mod_btn[i].classList.remove("modi_form");
+                rou_del_btn[i].style = "display:visible;";
+            }
+
+            var invisible = document.getElementsByClassName("no-modify");
+            for(let i = 0; i<invisible.length; i++) {
+                invisible[i].style.display = "none";
+            }
+            
+            var save = document.getElementById("rousave");
+            save.style = "display:visible; margin-top: 20px; float:right;";
+            
+            var plus_btn = document.getElementById("plus_btn");
+            plus_btn.style = "display:visible;";
+        }
+        
+    </script>
+          
+ 
+</div>
+<div class="col-md-1">
+</div>
