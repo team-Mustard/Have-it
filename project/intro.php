136c8bda65c778537e1b12b9e9ab5be67a6b473f
elephantLikesApple <elephant890@naver.com>
2021-10-05 08:24:23 +0900

    song-i modified intro UI.

--- project/intro.php
+++ project/intro.php
@@ -2,7 +2,6 @@
 <html lang="ko">
 <head>
     <meta charset="UTF-8">
-    <title>임시</title>
     <script data-require="jquery@*" data-semver="2.0.3" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
     <script data-require="bootstrap@*" data-semver="3.1.1" src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <link data-require="bootstrap-css@3.1.1" data-semver="3.1.1" rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />
@@ -12,33 +11,39 @@
 <body>
 <div class="container-fluid" id="top">
         <div class="sign text-center">
-            <img class="logo img-circle" src="img/logoRail.jpg">
+          <div class="logo">
+            <img class="img-circle" src="img/logoRail.png">
             <h1>' Have it '</h1>
-            <a class="btn purple" href="javascript:location.href='./login.php'">
+          </div>
+          <div class="line"></div>
+          <div class="buttons">
+            <a class="btn purple first" href="javascript:location.href='./login.php'">
                 <span style="font-size:1.3em;">로그인</span><br/>
                 <span>Login</span>
             </a>
-            <a class="btn pink" href="javascript:location.href='./signup.php'">
+            <br>
+            <a class="btn pink second" href="javascript:location.href='./signup.php'">
                 <span style="font-size:1.3em;">회원가입</span><br/>
                 <span>Sign up</span>
             </a>
+          </div>
         </div>
-            
+            <hr>
         <div class="problem row text-center">
           <div class="col-md-3"></div>
           <div class="col-md-6">
               <div class="col-md-4">
-                  <img class="problemIcon img-circle no-drag" src="./img/thumb_down.svg" alt="thumb-down" width="85px" height="85px">
+                  <img class="problemIcon img-circle" src="./img/thumb_down.svg" alt="thumb-down" width="85px" height="85px">
                   <p class="problemSub">날 지치게 하는</p>
                   <p class="problemTitle"><b>저조한 능률</b></p>
               </div>
               <div class="col-md-4">
-                  <img class="problemIcon img-circle no-drag" src="./img/timer_off.svg" alt="thumb-down" width="85px" height="85px">
+                  <img class="problemIcon img-circle" src="./img/timer_off.svg" alt="thumb-down" width="85px" height="85px">
                   <p class="problemSub">어떤 다짐도</p>
                   <p class="problemTitle"><b>작심 삼일</b></p>
               </div>
               <div class="col-md-4">
-                  <img class="problemIcon img-circle no-drag" src="./img/date_range.svg" alt="thumb-down" width="85px" height="85px">
+                  <img class="problemIcon img-circle" src="./img/date_range.svg" alt="thumb-down" width="85px" height="85px">
                   <p class="problemSub">자꾸만</p>
                   <p class="problemTitle"><b>놓치는 일정</b></p>
               </div>
@@ -47,12 +52,14 @@
         </div>
 
         <div class="solution text-center">
-          <div>거듭되는 실패 속<br>성공을 이끄는</div>
-          <img class="solutin diamond no-drag" alt="돌아가는 다이아몬드" src="./img/diamond.gif" width="300px">
-          <div>나만의 <strong>보물</strong>같은 습관 형성 일기!</div>
-          <div><strong>PDCA-cycle</strong>을 활용한 Have-it으로 시작하세요</div>
+          <h4>거듭되는 실패 속</h4>
+          <h4>성공을 이끄는</h4>
+          <h4>나만의 <strong>보물</strong>같은 습관 형성 일기!</h4>
+          <img class="diamond" alt="돌아가는 다이아몬드" src="./img/diamond.gif" width="120px">
+          <h3><strong>PDCA-cycle</strong>을 활용한 Have-it으로 시작하세요</h3>
         </div>
         
+        <div class="additional_space"></div>
         <div class="carousel PDCA text-center" id="carousel">
           <div class="prev">
             <img src="./img/action.png" alt="plan" width="150px">
@@ -69,7 +76,7 @@
         </div>
 
         <div class="footer text-center" style="margin-top:30px;margin-bottom:30px;">
-            <img class="logo img-circle" src="img/logoRail.jpg">
+            <img class="img-circle" src="img/logoRail.png">
             <h2> Developers : Team-Mustard</h2>
             <h6><i class="fab fa-github"></i> <a href="https://github.com/team-Mustard/Have-it">https://github.com/team-Mustard/Have-it</a></h6>
             <h6 class="loc"><i class="fas fa-map-marker-alt"></i> Department of Software, Chungbuk National University</h6>
@@ -155,5 +162,6 @@
     $(next).css('z-index', '4');
 
   }, 3000);
+
 </script>
 </html>
